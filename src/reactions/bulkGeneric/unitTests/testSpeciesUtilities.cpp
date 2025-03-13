2ffbbc428c86ba22e7972ee7ef503ab228a52f9c
Randolph Settgast <settgast1@llnl.gov>
2025-03-12 18:38:33 -0700

    rename utilities to SpeciesUtilities

--- src/reactions/bulkGeneric/unitTests/testSpeciesUtilities.cpp
+++ src/reactions/bulkGeneric/unitTests/testSpeciesUtilities.cpp
@@ -1,4 +1,4 @@
-#include "../utilities.hpp"
+#include "../SpeciesUtilities.hpp"
 #include "../ParametersPredefined.hpp"
 #include "common/printers.hpp"
 
@@ -6,6 +6,94 @@
 
 using namespace hpcReact;
 using namespace hpcReact::bulkGeneric;
+
+
+
+TEST( testUtilities, test_calculateLogSecondarySpeciesConcentration )
+{
+  constexpr int numReactions = carbonateSystem.numReactions;
+  constexpr int numSpecies = carbonateSystem.numSpecies;
+  constexpr int numPrimarySpecies = numSpecies - numReactions;
+  constexpr int numSecondarySpecies = numReactions;
+
+  double const logPrimarySpeciesSolution[numPrimarySpecies] =
+  {
+    log( 0.00043969547214915125 ),
+    log( 0.00037230096984514874 ),
+    log( 0.014716565308128551 ),
+    log( 0.0024913722747387217 ),
+    log( 1.8586090945989489 ),
+    log( 0.009881874292035079 ),
+    log( 1.0723078278653704 )
+  };
+
+  double logSecondarySpeciesConcentrations[numSecondarySpecies] = {0};
+
+  calculateLogSecondarySpeciesConcentration< double,
+                                             int,
+                                             int >( carbonateSystem,
+                                                    logPrimarySpeciesSolution,
+                                                    logSecondarySpeciesConcentrations );
+
+  double expectedSecondarySpeciesConcentrations[numSecondarySpecies] =
+  {
+    2.3278416955869804e-11,
+    0.3745973700632361,
+    3.956656978189425e-11,
+    0.0009629355924566718,
+    0.00006739226982791149,
+    1.065032288527949e-9,
+    0.005298329882666744,
+    0.005844517547638335,
+    0.012773193926706526,
+    0.006618125707964999,
+    0.01769217213462983
+  };
+
+  for( int j=0; j<numSecondarySpecies; ++j )
+  {
+    EXPECT_NEAR( exp( logSecondarySpeciesConcentrations[j] ),
+                 expectedSecondarySpeciesConcentrations[j],
+                 1.0e-8 );
+  }
+
+
+  double dLogSecondarySpeciesConcentrations_dLogPrimarySpeciesConcentrations[numSecondarySpecies][numPrimarySpecies] = {{0}};
+
+  calculateLogSecondarySpeciesConcentrationWrtLogC< double,
+                                                    int,
+                                                    int >( carbonateSystem,
+                                                           logPrimarySpeciesSolution,
+                                                           logSecondarySpeciesConcentrations,
+                                                           dLogSecondarySpeciesConcentrations_dLogPrimarySpeciesConcentrations );
+
+
+
+  double expected_dLogSdLogC[numSecondarySpecies][numPrimarySpecies] =
+  {
+    { -1, 0, 0, 0, 0, 0, 0 },
+    {  1, 1, 0, 0, 0, 0, 0 },
+    { -1, 1, 0, 0, 0, 0, 0 },
+    {  1, 1, 0, 0, 0, 0, 0 },
+    {  0, 1, 1, 0, 0, 0, 0 },
+    { -1, 1, 1, 0, 0, 0, 0 },
+    {  0, 0, 1, 1, 0, 0, 0 },
+    {  0, 0, 1, 0, 1, 0, 0 },
+    {  0, 0, 1, 0, 2, 0, 0 },
+    {  0, 0, 0, 1, 0, 1, 0 },
+    {  0, 0, 0, 1, 0, 0, 1 }
+  };
+
+  for( int i=0; i<numSecondarySpecies; ++i )
+  {
+    for( int j=0; j<numPrimarySpecies; ++j )
+    {
+      EXPECT_NEAR( dLogSecondarySpeciesConcentrations_dLogPrimarySpeciesConcentrations[i][j],
+                   expected_dLogSdLogC[i][j],
+                   1.0e-8 );
+    }
+  }
+}
 
 
 TEST( testUtilities, testcalculateAggregatePrimaryConcentrationsWrtLogC )
@@ -71,9 +159,6 @@ TEST( testUtilities, testcalculateAggregatePrimaryConcentrationsWrtLogC )
     { 0, 0, 0, 0.01769217213462971, 0, 0, 1.09 }
   };
 
-
-
-  print( dAggregatePrimarySpeciesConcentrationsDerivatives_dLogPrimarySpeciesConcentrations, "dAggregatePrimarySpeciesConcentrationsDerivatives_dLogPrimarySpeciesConcentrations", 4 );
 
   for( int i=0; i<numPrimarySpecies; ++i )
   {
