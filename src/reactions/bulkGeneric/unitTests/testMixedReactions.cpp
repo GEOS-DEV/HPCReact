56211c5f6f54c328fe09cf9b3476dbf3c13ab1c6
Matteo Cusini <matteo.cusini@gmail.com>
2025-06-03 11:11:19 -0700

    added pmpl

--- src/reactions/bulkGeneric/unitTests/testMixedReactions.cpp
+++ src/reactions/bulkGeneric/unitTests/testMixedReactions.cpp
@@ -5,6 +5,7 @@
 #include "common/macros.hpp"
 #include "common/printers.hpp"
 #include "common/nonlinearSolvers.hpp"
+#include "common/pmpl.hpp"
 
 #include <gtest/gtest.h>
 
@@ -30,97 +31,114 @@ void timeStepTest( PARAMS_DATA const & params,
 {
   HPCREACT_UNUSED_VAR( expectedSpeciesConcentrations );
 
-  using MixedReactionsType = MixedEquilibriumKineticReactions< REAL_TYPE,
+  CArrayWrapper< REAL_TYPE, PARAMS_DATA::numPrimarySpecies() > primarySpeciesConcentration;
+  
+  for( int i = 0; i < PARAMS_DATA::numPrimarySpecies(); ++i )
+  {
+    primarySpeciesConcentration[i] = initialSpeciesConcentration[i];
+  }
+
+  pmpl::genericKernelWrapper( PARAMS_DATA::numPrimarySpecies(), 
+                              primarySpeciesConcentration.data, 
+                              [=] HPCREACT_HOST_DEVICE ( auto * speciesConcentration ) 
+  { 
+    using MixedReactionsType = MixedEquilibriumKineticReactions< REAL_TYPE,
                                                                   int,
                                                                   int,
                                                                LOGE_CONCENTRATION >;
-  using EquilibriumReactionsType = EquilibriumReactions< REAL_TYPE,
-                                                         int,
-                                                         int >;
-
-  
-  // constexpr int numSpecies = PARAMS_DATA::numSpecies();
-  constexpr int numPrimarySpecies   = PARAMS_DATA::numPrimarySpecies();
-  constexpr int numSecondarySpecies = PARAMS_DATA::numSecondarySpecies();
-  constexpr int numKineticReactions = PARAMS_DATA::numKineticReactions();
+    using EquilibriumReactionsType = EquilibriumReactions< REAL_TYPE,
+                                                           int,
+                                                           int >;
+    
+    // constexpr int numSpecies = PARAMS_DATA::numSpecies();
+    constexpr int numPrimarySpecies   = PARAMS_DATA::numPrimarySpecies();
+    constexpr int numSecondarySpecies = PARAMS_DATA::numSecondarySpecies();
+    constexpr int numKineticReactions = PARAMS_DATA::numKineticReactions();
    
-  // define variables
-  double const temperature = 298.15;
-  REAL_TYPE logPrimarySpeciesConcentration[numPrimarySpecies];
-  // must use CArrayWrapper to ensure correct capture in the lambda functions
-  CArrayWrapper< REAL_TYPE, numSecondarySpecies > logSecondarySpeciesConcentration;
-  CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregatePrimarySpeciesConcentration;
-  CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregatePrimarySpeciesConcentration_n;
-  CArrayWrapper< REAL_TYPE, numPrimarySpecies, numPrimarySpecies > dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration;
-  CArrayWrapper< REAL_TYPE, numKineticReactions > reactionRates;
-  CArrayWrapper< REAL_TYPE, numKineticReactions, numPrimarySpecies > dReactionRates_dlogPrimarySpeciesConcentration;
-  CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregateSpeciesRates;
-  CArrayWrapper< REAL_TYPE, numPrimarySpecies, numPrimarySpecies > dAggregateSpeciesRates_dlogPrimarySpeciesConcentration;
+    // define variables
+    double const temperature = 298.15;
+    REAL_TYPE logPrimarySpeciesConcentration[numPrimarySpecies];
+    // must use CArrayWrapper to ensure correct capture in the lambda functions
+    CArrayWrapper< REAL_TYPE, numSecondarySpecies > logSecondarySpeciesConcentration;
+    CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregatePrimarySpeciesConcentration;
+    CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregatePrimarySpeciesConcentration_n;
+    CArrayWrapper< REAL_TYPE, numPrimarySpecies, numPrimarySpecies > dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration;
+    CArrayWrapper< REAL_TYPE, numKineticReactions > reactionRates;
+    CArrayWrapper< REAL_TYPE, numKineticReactions, numPrimarySpecies > dReactionRates_dlogPrimarySpeciesConcentration;
+    CArrayWrapper< REAL_TYPE, numPrimarySpecies > aggregateSpeciesRates;
+    CArrayWrapper< REAL_TYPE, numPrimarySpecies, numPrimarySpecies > dAggregateSpeciesRates_dlogPrimarySpeciesConcentration;
   
-
-  // Initialize species concentrations
-  for( int i = 0; i < numPrimarySpecies; ++i )
-  {
-    logPrimarySpeciesConcentration[i] = ::log( initialSpeciesConcentration[i] );
-    aggregatePrimarySpeciesConcentration[i] = initialSpeciesConcentration[i];
-  }
-  
-  EquilibriumReactionsType::enforceEquilibrium_Aggregate( temperature,
-                                                          carbonateSystem.equilibriumReactionsParameters(),
-                                                          logPrimarySpeciesConcentration,
-                                                          logPrimarySpeciesConcentration );
-                                                                                                         
-  
-  /// Time step loop 
-  double time = 0.0;
-  for( int t = 0; t < numSteps; ++t )
-  { 
-    printf( "Timestep %d, Time = %.6e\n", t, time );
-
-    for( int i=0; i < numPrimarySpecies; ++i )
+    // Initialize species concentrations
+    for( int i = 0; i < numPrimarySpecies; ++i )
     {
-      aggregatePrimarySpeciesConcentration_n[i] = aggregatePrimarySpeciesConcentration[i];
+      logPrimarySpeciesConcentration[i] = ::log( speciesConcentration[i] );
+      aggregatePrimarySpeciesConcentration[i] = speciesConcentration[i];
     }
+    
+    EquilibriumReactionsType::enforceEquilibrium_Aggregate( temperature,
+                                                            carbonateSystem.equilibriumReactionsParameters(),
+                                                            logPrimarySpeciesConcentration,
+                                                            logPrimarySpeciesConcentration );
+                                                                                                         
+    /// Time step loop 
+    double time = 0.0;
+    for( int t = 0; t < numSteps; ++t )
+    { 
+      printf( "Timestep %d, Time = %.6e\n", t, time );
 
-    auto computeResidualAndJacobian = [&] HPCREACT_HOST_DEVICE ( REAL_TYPE const (&logPrimarySpeciesConcentration)[numPrimarySpecies], 
+      for( int i=0; i < numPrimarySpecies; ++i )
+      {
+        aggregatePrimarySpeciesConcentration_n[i] = aggregatePrimarySpeciesConcentration[i];
+      }
+      
+      auto computeResidualAndJacobian = [&] HPCREACT_HOST_DEVICE ( REAL_TYPE const (&logPrimarySpeciesConcentration)[numPrimarySpecies], 
                                                                  REAL_TYPE (&r)[numPrimarySpecies],
                                                                  REAL_TYPE (&J)[numPrimarySpecies][numPrimarySpecies] )  
-    { 
-      MixedReactionsType::updateMixedSystem( temperature,
-                                             params,
-                                             logPrimarySpeciesConcentration,
-                                             logSecondarySpeciesConcentration,
-                                             aggregatePrimarySpeciesConcentration,
-                                             dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration,
-                                             reactionRates,
-                                             dReactionRates_dlogPrimarySpeciesConcentration,
-                                             aggregateSpeciesRates,
-                                             dAggregateSpeciesRates_dlogPrimarySpeciesConcentration );
+      { 
+        MixedReactionsType::updateMixedSystem( temperature,
+                                               params,
+                                               logPrimarySpeciesConcentration,
+                                               logSecondarySpeciesConcentration,
+                                               aggregatePrimarySpeciesConcentration,
+                                               dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration,
+                                               reactionRates,
+                                               dReactionRates_dlogPrimarySpeciesConcentration,
+                                               aggregateSpeciesRates,
+                                               dAggregateSpeciesRates_dlogPrimarySpeciesConcentration );
 
                                   
-      for ( int i = 0; i < numPrimarySpecies; ++i ) 
-      {
-        r[i] = ( aggregatePrimarySpeciesConcentration[i] - aggregatePrimarySpeciesConcentration_n[i] ) - aggregateSpeciesRates[i] * dt;
-        for ( int j = 0; j < numPrimarySpecies; ++j )
+        for ( int i = 0; i < numPrimarySpecies; ++i ) 
         {
-          J[i][j] = dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration[i][j] - dAggregateSpeciesRates_dlogPrimarySpeciesConcentration[i][j] * dt;
-        }
-      } 
-    };
+          r[i] = ( aggregatePrimarySpeciesConcentration[i] - aggregatePrimarySpeciesConcentration_n[i] ) - aggregateSpeciesRates[i] * dt;
+          for ( int j = 0; j < numPrimarySpecies; ++j )
+          {
+            J[i][j] = dAggregatePrimarySpeciesConcentrations_dlogPrimarySpeciesConcentration[i][j] - dAggregateSpeciesRates_dlogPrimarySpeciesConcentration[i][j] * dt;
+          }
+        }   
+      };
     
-    nonlinearSolvers::newtonRaphson< numPrimarySpecies >( logPrimarySpeciesConcentration, computeResidualAndJacobian );
+      nonlinearSolvers::newtonRaphson< numPrimarySpecies >( logPrimarySpeciesConcentration, computeResidualAndJacobian );
 
-    time += dt;
+      time += dt;
+    }
+    for(int i = 0; i < numPrimarySpecies; ++i )
+    {
+      speciesConcentration[i] = exp( logPrimarySpeciesConcentration[i] );
+    }
+  } );
+
+  // Check results
+  for(int i = 0; i < PARAMS_DATA::numPrimarySpecies(); ++i )
+  {
+    EXPECT_NEAR( primarySpeciesConcentration[ i ], expectedSpeciesConcentrations[ i ], 1.0e-8 * expectedSpeciesConcentrations[ i ]);
   }
-  
-  EXPECT_NEAR( reactionRatesDerivatives( r, i ), expectedReactionRatesDerivatives[r][i], std::max( magScale, fabs( expectedReactionRatesDerivatives[r][i] ) ) * 1.0e-8 );
 }
 
 TEST( testMixedReactions, testTimeStep_carbonateSystem )
 {
   constexpr int numPrimarySpecies = carbonateSystemType::numPrimarySpecies();
 
-  double const initialSpeciesConcentration[numPrimarySpecies] =
+  double const initialAggregateSpeciesConcentration[numPrimarySpecies] =
   {
     3.76e-3, // CaCO3 
     3.76e-1, // H+
@@ -134,20 +152,20 @@ TEST( testMixedReactions, testTimeStep_carbonateSystem )
 
   double const expectedSpeciesConcentrations[numPrimarySpecies] =
   {
-    1.0e-3,  // CaCO3
-    3.76e-1, // H+
-    3.76e-1, // HCO3-
-    3.87e-2, // Ca+2
-    3.21e-2, // SO4-2
-    1.89,    // Cl-
-    1.65e-2, // Mg+2
-    1.09     // Na+1
+    3.3318075516669661e-05,  // CaCO3
+    2.5894448848121536e-05, // H+
+    0.0062660162912796741, // HCO3-
+    0.015741214773567921, // Ca+2
+    0.0024602709470074127, // SO4-2
+    1.8564927944498291,    // Cl-
+    0.0099316034080546619, // Mg+2
+    1.0725251492409775     // Na+1
   };
 
   timeStepTest< double, true >( carbonateSystem,
                                 0.2,
                                 10,
-                                initialSpeciesConcentration,
+                                initialAggregateSpeciesConcentration,
                                 expectedSpeciesConcentrations );
 
 }
