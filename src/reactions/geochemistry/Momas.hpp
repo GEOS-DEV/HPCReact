b172c9677489ed3abd1f50e21eb73ecfb1406736
jkgolla <jongolla22@gmail.com>
2025-07-28 14:13:50 -0700

    Implement unit test for MoMaS benchmark easy case

---
+++ src/reactions/geochemistry/Momas.hpp
@@ -0,0 +1,93 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: (BSD-3-Clause)
+ *
+ * Copyright (c) 2025- Lawrence Livermore National Security LLC
+ * All rights reserved
+ *
+ * See top level LICENSE files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "../reactionsSystems/Parameters.hpp"
+
+namespace hpcReact
+{
+
+namespace geochemistry
+{
+// turn off uncrustify to allow for better readability of the parameters
+// *****UNCRUSTIFY-OFF******
+
+namespace momas
+{
+
+constexpr CArrayWrapper<double, 7, 12> stoichMatrix = 
+  {
+    // C1   C2   C3   C4   C5   CS1  CS2  |  X1  X2  X3   X4  S
+    { -1,   0,   0,   0,   0,   0,   0,      0,  -1,  0,  0,  0 },  // C1 = -X2
+    {  0,  -1,   0,   0,   0,   0,   0,      0,   1,  1,  0,  0 },  // C2 = X2 + X3
+    {  0,   0,  -1,   0,   0,   0,   0,      0,  -1,  0,  1,  0 },  // C3 = X2 + X4
+    {  0,   0,   0,  -1,   0,   0,   0,      0,  -4,  1,  3,  0 },  // C4 = -4X2 + X3 + 3X4
+    {  0,   0,   0,   0,  -1,   0,   0,      0,   4,  3,  1,  0 },  // C5 = 4X2 + 3X3 + X4
+    {  0,   0,   0,   0,   0,  -1,   0,      0,   3,  1,  0,  1 },  // CS1 = 3X2 + X3 + S
+    {  0,   0,   0,   0,   0,   0,  -1,      0,  -3,  0,  1,  2 }   // CS2 = -3X2 + X4 + 2S
+  };
+
+constexpr CArrayWrapper<double, 7> equilibriumConstants = 
+  { 
+    1.0e-12,   // C1 = -X2
+    1.0,       // C2 = X2 + X3
+    1.0,       // C3 = X2 + X4
+    0.1,       // C4 = -4X2 + X3 + 3X4
+    1.0e35,    // C5 = 4X2 + 3X3 + X4
+    1.0e6,     // CS1 = 3X2 + X3 + S
+    1.0e-1     // CS2 = -3X2 + X4 + 2S
+  };
+
+constexpr CArrayWrapper<double, 7> forwardRates = 
+  { 
+    0.0,   // C1 = -X2
+    0.0,   // C2 = X2 + X3
+    0.0,   // C3 = X2 + X4
+    0.0,   // C4 = -4X2 + X3 + 3X4
+    0.0,   // C5 = 4X2 + 3X3 + X4
+    0.0,   // CS1 = 3X2 + X3 + S
+    0.0    // CS2 = -3X2 + X4 + 2S
+  };
+
+constexpr CArrayWrapper<double, 7> reverseRates = 
+  { 
+    0.0,   // C1 = -X2
+    0.0,   // C2 = X2 + X3
+    0.0,   // C3 = X2 + X4
+    0.0,   // C4 = -4X2 + X3 + 3X4
+    0.0,   // C5 = 4X2 + 3X3 + X4
+    0.0,   // CS1 = 3X2 + X3 + S
+    0.0    // CS2 = -3X2 + X4 + 2S
+  };
+
+constexpr CArrayWrapper<int, 7> mobileSpeciesFlag = 
+  { 
+    1,   // C1 = -X2
+    1,   // C2 = X2 + X3
+    1,   // C3 = X2 + X4
+    1,   // C4 = -4X2 + X3 + 3X4
+    1,   // C5 = 4X2 + 3X3 + X4
+    0,   // CS1 = 3X2 + X3 + S
+    0    // CS2 = -3X2 + X4 + 2S
+  };
+  }
+  using momasSystemAllKineticType     = reactionsSystems::MixedReactionsParameters< double, int, int, 12, 7, 0 >;
+  using momasSystemAllEquilibriumType = reactionsSystems::MixedReactionsParameters< double, int, int, 12, 7, 7 >;
+  using momasSystemType               = reactionsSystems::MixedReactionsParameters< double, int, int, 12, 7, 7 >;
+
+  constexpr momasSystemAllKineticType momasSystemAllKinetic( momas::stoichMatrix, momas::equilibriumConstants, momas::forwardRates, momas::reverseRates, momas::mobileSpeciesFlag );
+  constexpr momasSystemAllEquilibriumType momasSystemAllEquilibrium( momas::stoichMatrix, momas::equilibriumConstants, momas::forwardRates, momas::reverseRates, momas::mobileSpeciesFlag );
+  constexpr momasSystemType carbonateSystem( momas::stoichMatrix, momas::equilibriumConstants, momas::forwardRates, momas::reverseRates, momas::mobileSpeciesFlag );
+
+// *****UNCRUSTIFY-ON******
+} // namespace geochemistry
+} // namespace hpcReact
---
+++ src/reactions/geochemistry/unitTests/testMomasEasyCase.cpp
@@ -0,0 +1,95 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: (BSD-3-Clause)
+ *
+ * Copyright (c) 2025- Lawrence Livermore National Security LLC
+ * All rights reserved
+ *
+ * See top level LICENSE files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+
+#include "reactions/unitTestUtilities/equilibriumReactionsTestUtilities.hpp"
+#include "../Momas.hpp"
+
+using namespace hpcReact;
+using namespace hpcReact::geochemistry;
+using namespace hpcReact::unitTest_utilities;
+
+
+// //******************************************************************************
+// TEST( testEquilibriumReactions, testEnforceEquilibrium )
+// {
+//   double const initialSpeciesConcentration[] = { 1.0, 1.0e-16, 0.5, 1.0, 1.0e-16 };
+//   double const expectedSpeciesConcentrations[5] = { 3.92138294e-01, 3.03930853e-01, 5.05945481e-01, 7.02014628e-01, 5.95970745e-01 };
+
+
+//   std::cout<<" RESIDUAL_FORM 2:"<<std::endl;
+//   testEnforceEquilibrium< double, 2 >( simpleTestRateParams.equilibriumReactionsParameters(),
+//                                        initialSpeciesConcentration,
+//                                        expectedSpeciesConcentrations );
+
+// }
+
+
+//******************************************************************************
+
+TEST( testEquilibriumReactions, testMoMasAllEquilibrium )
+{
+
+  using EquilibriumReactionsType = reactionsSystems::EquilibriumReactions< double,
+                                                                           int,
+                                                                           int >;
+
+  constexpr int numPrimarySpecies = hpcReact::geochemistry::momasSystemAllEquilibrium.numPrimarySpecies();
+
+  double const initialPrimarySpeciesConcentration[numPrimarySpecies] =
+  {
+    1.00e-20, // X1
+    2.00,     // X2
+    1.00e-20, // X3
+    2.00,     // X4
+    1.00      // S
+  };
+
+
+
+  double const logInitialPrimarySpeciesConcentration[numPrimarySpecies] =
+  {
+    log( initialPrimarySpeciesConcentration[0] ),
+    log( initialPrimarySpeciesConcentration[1] ),
+    log( initialPrimarySpeciesConcentration[2] ),
+    log( initialPrimarySpeciesConcentration[3] ),
+    log( initialPrimarySpeciesConcentration[4] )
+  };
+
+  double logPrimarySpeciesConcentration[numPrimarySpecies];
+  EquilibriumReactionsType::enforceEquilibrium_Aggregate( 0,
+                                                          hpcReact::geochemistry::momasSystemAllEquilibrium.equilibriumReactionsParameters(),
+                                                          logInitialPrimarySpeciesConcentration,
+                                                          logPrimarySpeciesConcentration );
+
+  double const expectedPrimarySpeciesConcentrations[numPrimarySpecies] =
+  {
+    9.7051090442170804E-21, // X1
+    5.0023298955833342E-12, // X2
+    1.9327372426296357E-33, // X3
+    7.3929274619958745E-12, // X4
+    9.8708294125907346E-13  // S
+  };
+
+  for( int r=0; r<numPrimarySpecies; ++r )
+  {
+    EXPECT_NEAR( exp( logPrimarySpeciesConcentration[r] ), expectedPrimarySpeciesConcentrations[r], 1.0e-8 * expectedPrimarySpeciesConcentrations[r] );
+  }
+
+
+}
+
+int main( int argc, char * * argv )
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  int const result = RUN_ALL_TESTS();
+  return result;
+}
