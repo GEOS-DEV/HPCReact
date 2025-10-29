ff25665311ce46c528b67eb559e19702282986e8
Randolph R. Settgast <settgast1@llnl.gov>
2025-10-28 23:42:23 -0700

    avoid optimization for coverage runs

--- scripts/ci_build_and_test_in_container.sh
+++ scripts/ci_build_and_test_in_container.sh
@@ -64,7 +64,6 @@ fi
 
 if [[ "$*" == *--code-coverage* ]]; then
   ENABLE_COVERAGE=ON
-  export LCOV_GCOV=/usr/bin/gcov-13
 else
   ENABLE_COVERAGE=OFF
 fi
@@ -104,7 +103,7 @@ fi
 
 if [[ "$*" == *--code-coverage* ]]; then
   or_die make -j ${NPROC} VERBOSE=1
-  LCOV_GCOV=/usr/bin/gcov-13 GENINFO_IGNORE_ERRORS=mismatch or_die make hpcReact_coverage
+  or_die make hpcReact_coverage
   cp -r ${HPCREACT_BUILD_DIR}/hpcReact_coverage.info.cleaned /tmp/hpcReact/hpcReact_coverage.info.cleaned
 fi
 
