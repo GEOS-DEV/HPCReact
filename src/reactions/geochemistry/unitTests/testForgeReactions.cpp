6ee5c5f19391d1c749b320dc35b5c7e1bb8b5bfe
Alex Zibitsker <zibitsker1@llnl.gov>
2026-01-27 16:02:37 -0800

    Minor comments and input data update in the forge test

--- src/reactions/geochemistry/unitTests/testForgeReactions.cpp
+++ src/reactions/geochemistry/unitTests/testForgeReactions.cpp
@@ -28,19 +28,34 @@ TEST( testForgeReactions, testTimeStep_forgeSystem )
     1.0, 1.0, 1.0, 1.0, 1.0 // SiO2(s), KAlMg3Si3O10(OH)2(s), CaAl2(SiO4)2(s), KAlSi3O8(s), Al2Si2O5(OH)4(s)
   };
 
+  // brine source
   double const initialAggregateSpeciesConcentration[numPrimarySpecies] =
   {
-    1.9964e-04, // H+
-    0.1050, // Ca+2
-    0.1209, // Mg+2
-    3.4000, // Na+
-    0.1250, // K+
-    5.4367e-04, // Al+++
-    2.60, // HCO3-
-    0.380, // SO4-2
-    1.0, // Cl-
-    0.0103 //SiO2(aq)
+    5.50E-06, // H+
+    2.75E-01, // Ca+2
+    1.21E-01, // Mg+2
+    3.41E+00, // Na+
+    3.25E-02, // K+
+    5.45E-04, // Al+++
+    2.60E+00, // HCO3-
+    3.81E-01, // SO4-2
+    1.00E+00, // Cl-
+    1.03E-02 //SiO2(aq)
   };
+  //brine, from sink at t=0.23h
+  //   double const initialAggregateSpeciesConcentration[numPrimarySpecies] =
+  // {
+  //   0.01202, // H+
+  //   9.31E-02, // Ca+2
+  //   1.36E-01, // Mg+2
+  //   3.40, // Na+
+  //   2.38E-01, // K+
+  //   2.93E-03, // Al+++
+  //   2.60, // HCO3-
+  //   3.80E-01, // SO4-2
+  //   1.00E+00, // Cl-
+  //   2.79E-01 //SiO2(aq)
+  // };
 
   double const expectedSpeciesConcentrations[numPrimarySpecies] =
   {
@@ -57,8 +72,8 @@ TEST( testForgeReactions, testTimeStep_forgeSystem )
   };
 
   timeStepTest< double, true >( forgeSystem,
-                                1.e-5,
-                                10,
+                                1.e-7,
+                                30,
                                 initialAggregateSpeciesConcentration,
                                 surfaceArea,
                                 expectedSpeciesConcentrations );
